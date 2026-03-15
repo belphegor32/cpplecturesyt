43986f4931e47c95826058ef47cdf3be88c78e52
Ilya Ishutkin <ilia.ishutkin@gmail.com>
2026-03-15 15:54:37 +0300

    for tomorrow, maybe

--- 1_5_control.cpp
+++ 1_5_control.cpp
@@ -20,18 +20,18 @@ int main(){
 
     // } while ();
     
-    for(declaration | expression; bool-expression ; expression){
-        break; // выйди из цикла
-        continue; // перескочи на следующую итерацию
-    }
+//     for(declaration | expression; bool-expression ; expression){
+//         break; // выйди из цикла
+//         continue; // перескочи на следующую итерацию
+//     }
 
 
 
-    ++x;
-    std::cout << x;
-    goto label;
+//     ++x;
+//     std::cout << x;
+//     goto label;
 
-label: 
+// label: 
 
     return 0;
 }
---
+++ 1_6_ub.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// 1.6. Compile-time errors, runtime errors and undefined behavior.
+int main(){
+
+
+    return 0;
+}
