8d4bb652844db40fbe0b5977895c458226e34266
Ilya Ishutkin <ilia.ishutkin@gmail.com>
2026-03-15 15:49:54 +0300

    hello

---
+++ 1_5_control.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(){
+    
+    int x;
+    std::cin >> x;
+    // switch(x){
+    // case 1:
+    //     std::cout << "AAAA";
+    //     break;
+    // case 2:
+    //     std::cout << "BBBB";
+    //     break;
+    // default:
+    //     std::cout << "CCCC";
+    //     break;
+    // }
+
+    // do {
+
+    // } while ();
+    
+    for(declaration | expression; bool-expression ; expression){
+        break; // выйди из цикла
+        continue; // перескочи на следующую итерацию
+    }
+
+
+
+    ++x;
+    std::cout << x;
+    goto label;
+
+label: 
+
+    return 0;
+}
